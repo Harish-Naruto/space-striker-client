d42b670d0b491d93edb9af1525ca85b519833582
Harish-Naruto <harishnarote7@gmail.com>
2026-01-29 23:35:40 +0530

    inital setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from '@/components/ui/button'
+
+function App() {
+
+  return (
+    <>
+       <h1 className="text-3xl font-bold underline bg-amber-700">
+        Hello world!
+      </h1>
+      <Button>click me</Button>
+    </>
+  )
+}
+
+export default App
